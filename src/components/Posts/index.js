b594e4729b58001ad23c4a9a53467e0e49399d99
aoc <michaelmoranesul@gmail.com>
2023-06-06 18:56:48 -0300

    usando componentes do react e a funcao map do javascript para criar os posts dinamicamente

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,23 +1,55 @@
-import posts from "json/post.json"
+import {
+  Card,
+  CardBody,
+  Stack,
+  Heading,
+  Text,
+  Image,
+  CardFooter,
+  Button
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-function Posts() {
- return (
-   <div>
-    {posts.map((item) => (
-     <div key={item.id}>
-      <Link>
-        <div>
-         div da imagem
-        </div>
-        <div>
-         div do texto
-        </div>
-      </Link>
-     </div>
-    ))}
-   </div>
- )
+function Posts(props) {
+  const { posts } = props;
+  return (
+    <>
+     {posts.map((item) => (
+       <Card
+       direction={{ base: "column", sm: "row" }}
+       overflow="hidden"
+       variant="outline"
+       mb="1em"
+       w={{base: "100%", sm: "auto"}}
+       key={item.id}
+     >
+       <Image
+         objectFit="cover"
+         maxW={{ base: "100%", sm: "200px" }}
+         src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+         alt="Caffe Latte"
+       />
+       <Stack>
+         <CardBody>
+           <Heading>
+             {item.text}
+           </Heading>
+           <Text py="2">
+             Desenvolvido por MichaelMoranis
+           </Text>
+         </CardBody>
+         <CardFooter>
+           <Button variant="solid" colorScheme="blue">
+             <Link>
+               ver mais
+             </Link>
+           </Button>
+         </CardFooter>
+       </Stack>
+     </Card>
+     ))}
+    </>
+  );
 }
 
 export default Posts;
